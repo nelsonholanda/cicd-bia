b177bae231559601529030ae552cc1d7f3c89c3e
nelsonholanda <nelsonholanda@hotmail.com>
2025-06-16 14:23:52 -0300

    MUDANCA DE CORES

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -10,8 +10,8 @@ const Header = ({ title, onAdd, showAdd }) => {
       {/* <h3>process.env.EMAIL_ALUNO</h3> */}
       {location.pathname === "/" && (
         <Button
-          color={showAdd ? "yellow" : "grey"}
-          text={showAdd ? "Fechar" : "Adicionar"}
+          color={showAdd ? "green" : "black"}
+          text={showAdd ? "Close" : "Add"}
           onClick={onAdd}
         />
       )}
