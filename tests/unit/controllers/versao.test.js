affc3886655804c5d73399146f86c3788e64e998
nelsonholanda <nelsonholanda@hotmail.com>
2025-06-24 14:15:26 -0300

    update build

--- tests/unit/controllers/versao.test.js
+++ tests/unit/controllers/versao.test.js
@@ -34,13 +34,13 @@ describe('Versao Controller', () => {
 
   test('get deve retornar a string de resposta correta quando VERSAO_API está definido', () => {
     // Simula o cenário onde VERSAO_API está definido
-    process.env.VERSAO_API = '1.0.0';
+    process.env.VERSAO_API = '1.0.1';
 
     // Chama a função retornada pelo controller para obter o objeto controller
     const { get } = versaoController();
     // Chama o método get do objeto controller
     get(req, res);
 
-    expect(res.send).toHaveBeenCalledWith('Bia 1.0.0');
+    expect(res.send).toHaveBeenCalledWith('Bia 1.0.1');
   });
 });
